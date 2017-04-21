24688d4e0453dae1ef2550db0d0eecf1fa92b8cf
MalgAmoe <malgamoe@gmail.com>
2017-04-21 16:56:52 +0200

    chore: created a sequencer class

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 var audioContext = new AudioContext()
 
-var interval = setInterval(playKicks, 4000);
+//------------------------global variables
+var tempo = 90
+var eventsScheduled = []
 
-function playKicks () {
-  playKick(0, 0.05)
-  playKick(1, 0.05)
-  playKick(2, 0.05)
-  playKick(3, 0.05)
+//------------------------sequencer class
+function Sequencer(resolution, division) {
+  var seq = {}
+  seq.resolution = resolution
+  seq.division = division
+  seq.steps = []
+  seq.currentStep = -1
+  for (var i = 0; i < division; i++) {
+    seq.steps[i] = 0
+  }
+
+  seq.setStep = function(stepNum, velocity) {
+    seq.steps[stepNum] = velocity
+  }
+
+  seq.eraseStep = function(stepNum) {
+    seq.setStep(stepNum, 0)
+  }
+
+  seq.checkNextStep = function() {
+    var nextStep = ++seq.currentStep
+    if (nextStep >= seq.division) {
+      nextStep = seq.currentStep = 0
+    }
+    var velocity = seq.steps[nextStep]
+    var nextStepData = [nextStep, velocity]
+    console.log(nextStepData);
+    return nextStepData
+  }
+
+  return seq
 }
 
-function playKick (delay, duration) {
+//-------------------------sequencer instance
+var kickSequencer = Sequencer(1, 16)
+kickSequencer.setStep(0, 1)
+kickSequencer.setStep(3, 0.5)
+kickSequencer.setStep(4, 1)
+kickSequencer.setStep(6, 0.8)
+kickSequencer.setStep(8, 0.5)
+kickSequencer.setStep(10, 1)
+kickSequencer.setStep(12, 1)
+
+console.log('seq', kickSequencer.steps);
+
+//-------------------------event scheduling
+var intervalCheck = setInterval(kickSequencer.checkNextStep, 500)
+
+var interval = setInterval(playKicks, 4000)
+
+function playKicks() {
+  playKick(0, 0.05, 1)
+  playKick(1, 0.05, 0.3)
+  playKick(2, 0.05, 0.8)
+  playKick(3, 0.05, 0.2)
+}
+
+//-------------------------sound generation
+function playKick (delay, duration, velocity) {
   var startTime = audioContext.currentTime + delay
   var endTime = startTime + duration
 
@@ -24,7 +77,7 @@ function playKick (delay, duration) {
   amp.connect(audioContext.destination)
   oscillator.connect(amp)
   amp.gain.value = 0
-  amp.gain.setTargetAtTime(1, startTime, 0.0001)
+  amp.gain.setTargetAtTime(velocity, startTime, 0.0001)
   amp.gain.setTargetAtTime(0, endTime, 0.2)
 
   oscillator.start(startTime)
