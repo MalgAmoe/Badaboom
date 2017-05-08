49fc57b179468a5111b1c7fa7a528e29bbd1a8eb
MalgAmoe <malgamoe@gmail.com>
2017-05-08 13:08:01 +0200

    feat: change slider to be bigger

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import ReactGA from 'react-ga'
 import SequencerControl from './containers/SequencerControl'
 import SoundControl from './containers/SoundControl'
